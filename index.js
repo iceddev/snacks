fddb5fed40bd051e4d11f75a970692cf75b211fe
Blaine Bublitz <blaine@iceddev.com>
2015-03-05 01:13:37 -0700

    initial impl

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var levelup = require('levelup');
+var leveldown = require('leveldown');
+var Immstruct = require('immstruct').Immstruct;
+
+var db = levelup('snacks', { db: leveldown, valueEncoding: 'json' });
+
+function snacks(app, opts, cb){
+
+  var loaded = false;
+
+  var imm = new Immstruct();
+  var structure = imm.get('userConfig', {});
+
+  var config = structure.cursor();
+
+  var namespace = opts.namespace || 'userConfig';
+
+  app.expose(namespace, config);
+
+  function onSwap(n, o, path){
+    config = structure.cursor();
+    app[namespace] = config;
+
+    // don't proceed to saving if we aren't loaded
+    if(!loaded){
+      return;
+    }
+
+    // fire off in the background
+    db.put(path, structure.cursor(path).deref());
+  }
+
+  structure.on('swap', onSwap);
+
+  db.createReadStream()
+    .on('data', function(data){
+      config.set(data.key, data.value);
+    })
+    .on('end', function(){
+      loaded = true;
+      cb();
+    });
+}
+
+module.exports = snacks;
